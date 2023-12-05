57f10a0676ee44e1ddf616dc064202beebb9c28a
RasulYakubov <82101018+RasulYakubov@users.noreply.github.com>
2023-12-05 20:21:26 +0500

    fix

--- pages/auth/register.vue
+++ pages/auth/register.vue
@@ -1,3 +1,8 @@
+<script lang="ts" setup>
+import { getRegister_DEFAULT, type RegisterModel } from '~/services/profile';
+const _formData = ref<RegisterModel>(getRegister_DEFAULT())
+</script>
+
 <template>
     <NuxtLayout name="auth">
         <section>
@@ -5,13 +10,13 @@
                 <h1 class="font-commissioner-700 text-4xl text-center">Регистрация в системе</h1>
                 <form class="mt-10">
                     <div class="grid grid-cols-2 gap-6">
-                        <BaseInput label="Имя" placeholder="Введите имя" />
-                        <BaseInput label="Фамилия" placeholder="Введите фамилию" />
-                        <BaseInput label="Организация" placeholder="Введите название организации" />
-                        <BaseInput label="ИНН организации" placeholder="Введите ИНН" />
-                        <BaseInput label="Номер телефона" placeholder="Введите номер телефона" />
+                        <BaseInput v-model="_formData.name" label="Имя" placeholder="Введите имя" />
+                        <BaseInput v-model="_formData.surname" label="Фамилия" placeholder="Введите фамилию" />
+                        <BaseInput v-model="_formData.companyName" label="Организация" placeholder="Введите название организации" />
+                        <BaseInput v-model="_formData.companyType" label="ИНН организации" placeholder="Введите ИНН" />
+                        <BaseInput v-model="_formData.mobile" label="Номер телефона" placeholder="Введите номер телефона" />
                         <BaseInput label="Электронная почта" placeholder="Введите электронную почту" />
-                        <BaseInput label="Пароль" placeholder="Введите пароль" />
+                        <BaseInput v-model="_formData.password" label="Пароль" placeholder="Введите пароль" />
                         <BaseInput label="Повторите пароль" placeholder="Повторите пароль" />
                     </div>
                     <div class="mt-10 flex justify-center">
